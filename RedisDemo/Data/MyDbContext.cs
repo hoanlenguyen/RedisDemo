a2544f7506ce88c48c94fe2fb77163c6ec93f31c
hoan.le <hoan.le@saigontechnology.com>
2022-11-25 17:29:59 +0700

    sync code

---
+++ RedisDemo/Data/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedisDemo.Models;
+
+namespace RedisDemo.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ArticleMatrix> ArticleMatrices { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ RedisDemo/Models/Feed.cs
@@ -0,0 +1,12 @@
+﻿namespace RedisDemo.Models
+{
+    public class Feed
+    {
+        public string Link { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string FeedType { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime PubDate { get; set; } = DateTime.Now;
+    }
+}
